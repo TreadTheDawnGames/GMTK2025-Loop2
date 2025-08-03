6b3db43ba14d0e2e4d0062c67c4b55857e1d2d15
GANONDORF\CASPI <caspian@tylerinternet.com>
2025-08-03 01:54:50 -0400

    Nebula

--- Scripts/rand_sprite.gd
+++ Scripts/rand_sprite.gd
@@ -3,9 +3,13 @@ class_name RandSprite2D
 @export var PlanetSprites : Array[Texture2D]
 const CIRCLE_NORMAL_MAP = preload("res://Assets/Shaders/CircleNormalMap.png")
 @export var normalMap : Array[Texture2D] 
-
+@export var noNormalMap : bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if(noNormalMap):
+		texture = PlanetSprites.pick_random()
+		return
+	
 	rotation_degrees = randi()%360
 	var canvas = CanvasTexture.new()
 	canvas.diffuse_texture = PlanetSprites.pick_random()
